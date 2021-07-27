4a53ff41255943d0307fa9a301daa656dab5d7ab
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 22:22:55 +0530

    Uc2 Uploading

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -6,14 +6,24 @@ public class EmpWageBuilder {
 		// TODO Auto-generated method stub
      System.out.println("Welcome to EmpWage Computation");
      
-     int is_present = 1;
+     int is_fulltime_present = 1;
+		int daily_wage;
+		int wage_per_hr = 20;
+		int working_hr;
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
-		if( emp_check == is_present)
+		if( emp_check == is_fulltime_present)
 		{
 			System.out.println("Employee is present");
+			working_hr = 8; 
 		}
 		else
+		{
 			System.out.println("Employee is absent");
-	}
+			working_hr = 0;
+		}
+		
+		daily_wage = working_hr * wage_per_hr;
+		System.out.println("Employee daily wage is " + daily_wage);
 
+}
 }
