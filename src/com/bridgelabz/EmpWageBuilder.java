987c107a97796a853107823ecdf55758d179ecc5
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 22:05:03 +0530

    Uc1 uploading

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -5,6 +5,15 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Welcome to EmpWage Computation");
+     
+     int is_present = 1;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+		if( emp_check == is_present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
