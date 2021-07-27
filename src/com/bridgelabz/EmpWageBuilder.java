ff97de4d67c0068389754a83a5d0a1bee810cc74
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 22:26:59 +0530

    Uc3 Uploading

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -7,14 +7,20 @@ public class EmpWageBuilder {
      System.out.println("Welcome to EmpWage Computation");
      
      int is_fulltime_present = 1;
+		int is_parttime_present = 2;
 		int daily_wage;
 		int wage_per_hr = 20;
 		int working_hr;
-		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
 		if( emp_check == is_fulltime_present)
 		{
-			System.out.println("Employee is present");
+			System.out.println("Employee is full time present");
 			working_hr = 8; 
+		}
+		else if( emp_check == is_parttime_present)
+		{
+			System.out.println("Employee is part time present");
+			working_hr = 4;
 		}
 		else
 		{
@@ -24,6 +30,5 @@ public class EmpWageBuilder {
 		
 		daily_wage = working_hr * wage_per_hr;
 		System.out.println("Employee daily wage is " + daily_wage);
-
 }
 }
